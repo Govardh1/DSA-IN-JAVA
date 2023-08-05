3fc9d57aa11992c293a6f326a6690fbe84689f6f
Govardhan_KG <govi.reddy420@gmail.com>
2023-08-05 21:23:56 +0530

    day-1

---
+++ BinarySearch/SmallestLetter.java
@@ -0,0 +1,21 @@
+public class SmallestLetter {
+    public static void main(String[] args) {
+       char[] letters={'c','f','j','m'} ;
+       char target='j';
+        char ans=SmallestLetter(letters,target);
+        System.out.println(ans);
+    }
+    static char SmallestLetter(char[] letters,char target){
+       int start=0;
+       int end=letters.length-1;
+       while(start<=end){
+        int mid=start+(end-start)/2;
+        if(target>=letters[mid]){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+       }
+        return(letters[start% letters.length]);
+    }
+}
