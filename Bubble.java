e736bce51d221ace3da82729aad8a8aa67e65e8b
Govardhan_KG <govi.reddy420@gmail.com>
2023-08-09 20:35:50 +0530

    day-2

---
+++ Bubble.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Bubble{
+  public static void main(String[] args) {
+    int[] arr={3,1,5,4,2};
+    int[] ans=BubbleSort(arr);
+    System.out.println(Arrays.toString(ans));
+  } 
+  static int[] BubbleSort(int[] arr){
+    for(int i=0;i<arr.length-1;i++){
+        for(int j=0;j<arr.length-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    return arr;
+  } 
+}
